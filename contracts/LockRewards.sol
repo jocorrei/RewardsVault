db13c24c3c69d8e61760e78c1b1f9a7898d8b87c
Gabriel Contarini <gcontarini@gmail.com>
2022-05-16 22:38:38 +0100

    Implement basic deposit with rewards update

--- contracts/LockRewards.sol
+++ contracts/LockRewards.sol
@@ -14,65 +14,48 @@ contract LockRewards is ReentrancyGuard {
         uint256 balance;
         uint256 lockDate;
         uint256 lockPeriod;
-        uint256 lockEpochs;
+        // uint256 lockEpochs;
+        uint256 lastEpochPaid;
         uint256 rewards1;
         uint256 rewards2;
     }
 
-    struct Total {
-        uint256 managed;
-        uint256 locked;
-        uint256 rewardPerToken1;
-        uint256 rewardPerToken1;
-    }
-
-    // Mapping to a struct, the end or start date is the 
-    // key and the struct has the info to distribute rewards
     struct Epoch {
+        mapping(address => uint256) balanceLocked;
         uint256 start;
         uint256 finish;
-        uint256 epoch;
-        bool    status;
+        uint256 totalLocked;
+        uint256 rewards1;
+        uint256 rewards2;
+        bool    isSet;
+        // bool    isCurrent;
     }
+    
+    /* ========== STATE VARIABLES ========== */
 
-    // Array to store the rewards paid, account has an index in which tells where to for loop
+    mapping(address => Account) public accounts;
+    mapping(uint256 => Epoch) public epochs;
+    uint256 public currentEpoch = 0;
+    uint256 public nextUnsetEpoch = 1;
+
     address public lockToken;
     address public rewardToken1;
     address public rewardToken2;
-    uint256 public epoch = 7 days;
+    uint256 public epochDuration = 7 days;
+    uint256 public totalAssets;
     
-    Epoch public currentEpoch;
-    Total public total;
-    mapping(address => Account) public accounts;
-
-    /* ========== STATE VARIABLES ========== */
-
-    IERC20 public rewards1;
-    IERC20 public stakingToken;
-    uint256 public periodFinish = 0;
-    uint256 public rewardRate = 0;
-    uint256 public rewardsDuration = 7 days;
-    uint256 public lastUpdateTime;
-    uint256 public rewardPerTokenStored;
-
-    mapping(address => uint256) public userRewardPerTokenPaid;
-    mapping(address => uint256) public rewards;
-
-    uint256 private _totalSupply;
-    mapping(address => uint256) private _balances;
-
     /* ========== CONSTRUCTOR ========== */
 
-    constructor(
-        address _owner,
-        address _rewardsDistribution,
-        address _rewardsToken,
-        address _stakingToken
-    ) public Owned(_owner) {
-        rewardsToken = IERC20(_rewardsToken);
-        stakingToken = IERC20(_stakingToken);
-        rewardsDistribution = _rewardsDistribution;
-    }
+    // constructor(
+    //     address _owner,
+    //     address _rewardsDistribution,
+    //     address _rewardsToken,
+    //     address _stakingToken
+    // ) public Owned(_owner) {
+    //     rewardsToken = IERC20(_rewardsToken);
+    //     stakingToken = IERC20(_stakingToken);
+    //     rewardsDistribution = _rewardsDistribution;
+    // }
 
     /* ========== VIEWS ========== */
 
@@ -108,55 +91,61 @@ contract LockRewards is ReentrancyGuard {
 
     /* ========== MUTATIVE FUNCTIONS ========== */
 
-    // function deposit(uint256 amount, uint256 lockTime) public nonReentrant updateReward(msg.sender) {
-    //     if (amount <= 0) revert InsufficientAmount();
-    //     IERC20 lToken = IERC20(lockToken);
-    //     uint256 unlockDate = accounts[msg.sender].lockDate + accounts[msg.sender].lockPeriod;
-    //     uint256 newUnlockdate = block.timestamp + lockTime;
-
-    //     accounts[msg.sender].balance += amount;
-    //     // Ja deu ruim aqui, isso nao funciona
-    //     // Pq eu preciso quanto foi depositado e quando pra dar o valor certo de reward.
-    //     if (unlockDate < newUnlockdate) {
-    //         accounts[msg.sender].
-    //     }
-    //     accounts[msg.sender].lockDate = block.timestamp;
-    //     total.managed += amount;
-        
-    //     // Probably not right
-    //     lToken.safeTransferFrom(msg.sender, address(this), amount);
-    //     emit LockTokens(msg.sender, amount, block.timestamp + lockTime);
-    // }
-
-    function withdraw(uint256 amount) public nonReentrant updateReward(msg.sender) {
-        if (amount <= 0 || accounts[msg.sender].balance < amount) revert InsufficientAmount();
-        
-        uint256 unlockDate = accounts[msg.sender].lockDate + accounts[msg.sender].lockPeriod;
-        if (unlockDate > block.timestamp) revert FundsInLockPeriod();
-
+    // Haven't implemented relock case yet
+    // Define Deposit event
+    function deposit(uint256 amount, uint256 lockTime) public nonReentrant updateEpoch updateReward(msg.sender) {
+        if (amount <= 0) revert InsufficientAmount();
         IERC20 lToken = IERC20(lockToken);
+        uint256 next = currentEpoch + 1;
+        
+        accounts[msg.sender].lockDate = block.timestamp;
+        accounts[msg.sender].lockPeriod = lockTime;
 
-        accounts[msg.sender].balance -= amount;
-        accounts[msg.sender].lockDate = 0;
+        uint256 lockEpochs =  lockTime / epochDuration;
+        accounts[msg.sender].lockEpochs = lockEpochs;
+        
+        lToken.safeTransferFrom(msg.sender, address(this), amount);
+        totalAssets += amount;
 
-        lToken.safeTransfer(msg.sender, amount);
-        emit Withdrawn(msg.sender, amount);
+        for (uint256 i = 0; i < lockEpochs; i++) {
+            epochs[i + next].totalLocked += amount;
+            epochs[i + next].balanceLocked[msg.sender] += amount;
+        }
+        accounts[msg.sender].balance += amount;
+        emit Deposit(msg.sender, amount, block.timestamp + lockTime);
     }
 
-    function claimReward() public nonReentrant updateReward(msg.sender) {
+    // Old bullshit
+    // function withdraw(uint256 amount) public nonReentrant updateReward(msg.sender) {
+    //     if (amount <= 0 || accounts[msg.sender].balance < amount) revert InsufficientAmount();
+        
+    //     uint256 unlockDate = accounts[msg.sender].lockDate + accounts[msg.sender].lockPeriod;
+    //     if (unlockDate > block.timestamp) revert FundsInLockPeriod();
+
+    //     IERC20 lToken = IERC20(lockToken);
+
+    //     accounts[msg.sender].balance -= amount;
+    //     accounts[msg.sender].lockDate = 0;
+
+    //     lToken.safeTransfer(msg.sender, amount);
+    //     emit Withdrawn(msg.sender, amount);
+    // }
+
+    function claimReward() public nonReentrant updateEpoch updateReward(msg.sender) {
         uint256 reward1 = accounts[msg.sender].reward1;
         uint256 reward2 = accounts[msg.sender].reward2;
+
         if (reward1 > 0) {
             IERC20 rToken1 = IERC20(reward1);
             account[msg.sender].reward1 = 0;
-            rToken1.safeTransfer(msg.sender, reward);
-            emit RewardPaid(msg.sender, rewardToken1, reward);
+            rToken1.safeTransfer(msg.sender, reward1);
+            emit RewardPaid(msg.sender, rewardToken1, reward1);
         }
         if (reward2 > 0) {
             IERC20 rToken2 = IERC20(reward2);
             account[msg.sender].reward2 = 0;
-            rToken2.safeTransfer(msg.sender, reward);
-            emit RewardPaid(msg.sender, rewardToken2, reward);
+            rToken2.safeTransfer(msg.sender, reward1);
+            emit RewardPaid(msg.sender, rewardToken2, reward1);
         }
     }
 
@@ -167,49 +156,73 @@ contract LockRewards is ReentrancyGuard {
 
     /* ========== RESTRICTED FUNCTIONS ========== */
 
-    function setNextEpoch(uint256 reward1, uint256 reward2) external onlyRewardsDistribution updateReward(address(0)) {
-        // Check if epoch is current right now
-        // Then set the next one
-        // Calculate the reward per token for this epoch
-        // Set the epoch stuff
-        if (nextEpoch.status) revert NextEpochAlreadySet();
+    function setNextEpoch(uint256 reward1, uint256 reward2) external onlyOwner updateEpoch {
+        uint256 next = nextUnsetEpoch;
 
-        nextEpoch.start = block.timestamp;
-        nextEpoch.finish = block.timestamp + epoch;
-        nextEpoch.reward1PerToken = reward1 / total.Locked;
-        nextEpoch.reward2PerToken = reward2 / total.Locked;
+        uint256 _now = block.timestamp;
+        uint256 finish = epochs[next - 1].finish;
+        if (finish < _now) {
+            epochs[next].start = _now;
+        } else {
+            epochs[next].start = finish;
+        }
+
+        epochs[next].finish = epochs[next].start + epochDuration;
+        // Should check balance of contract for reward?
+        epochs[next].reward1 = reward1;
+        epochs[next].reward2 = reward2;
+        epochs[next].isSet = true;
+        
+        nextUnsetEpoch += 1;
+        emit setNextReward(next, reward1, reward2, epoch[next].start, epoch[next].finish);
     }
 
-    // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
-    function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
-        require(tokenAddress != address(stakingToken), "Cannot withdraw the staking token");
-        IERC20(tokenAddress).safeTransfer(owner, tokenAmount);
-        emit Recovered(tokenAddress, tokenAmount);
-    }
+    // // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
+    // function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
+    //     require(tokenAddress != address(stakingToken), "Cannot withdraw the staking token");
+    //     IERC20(tokenAddress).safeTransfer(owner, tokenAmount);
+    //     emit Recovered(tokenAddress, tokenAmount);
+    // }
 
-    function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
-        require(
-            block.timestamp > periodFinish,
-            "Previous rewards period must be complete before changing the duration for the new period"
-        );
-        rewardsDuration = _rewardsDuration;
-        emit RewardsDurationUpdated(rewardsDuration);
-    }
+    // function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
+    //     require(
+    //         block.timestamp > periodFinish,
+    //         "Previous rewards period must be complete before changing the duration for the new period"
+    //     );
+    //     rewardsDuration = _rewardsDuration;
+    //     emit RewardsDurationUpdated(rewardsDuration);
+    // }
 
     /* ========== MODIFIERS ========== */
+    
+    // Think about edge case like the last valid epoch is due
+    // But owner wants to set new rewards again. What will happen?
+    modifier updateEpoch {
+        uint256 current = currentEpoch;
+
+        if (epochs[current].finish <= block.timestamp && epochs[current].isSet == true)
+            currentEpoch += 1;
+        _;
+    }
 
     modifier updateReward(address account) {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = lastTimeRewardApplicable();
-        if (account != address(0)) {
-            rewards[account] = earned(account);
-            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        uint256 current = currentEpoch;
+        uint256 lastEpochPaid = accounts[msg.sender].lastEpochPaid;
+
+        for (int i = lastEpochPaid; i < current; i++) {
+            uint256 share = epochs[i].balanceLocked[msg.sender] * 1e18 / epochs[i].totalLocked;
+
+            accounts[msg.sender].reward1 += share * epochs[i].reward1 / 1e18;
+            accounts[msg.sender].reward2 += share * epochs[i].reward2 / 1e18;
         }
+
+        accounts[msg.sender].lastEpochPaid = current;
         _;
     }
 
     /* ========== EVENTS ========== */
 
+    // Create more events and delete unused ones
     event RewardAdded(uint256 reward);
     event Staked(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount);
