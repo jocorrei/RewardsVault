e3ccaf5e45b8f8d40b8ddf04582bbb16328e84a7
jocorrei <mgjoao41@gmail.com>
2022-05-20 11:08:09 +0100

    merging

--- contracts/LockRewards.sol
+++ contracts/LockRewards.sol
@@ -192,13 +192,6 @@ contract LockRewards is ReentrancyGuard {
         emit RecoveredNFT(tokenAddress, tokenId);
     }
 
-    // // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
-    // function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
-    //     require(tokenAddress != address(stakingToken), "Cannot withdraw the staking token");
-    //     IERC20(tokenAddress).safeTransfer(owner, tokenAmount);
-    //     emit Recovered(tokenAddress, tokenAmount);
-    // }
-
     // function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
     //     require(
     //         block.timestamp > periodFinish,
